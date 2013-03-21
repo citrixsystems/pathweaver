cba5887bdf9e91cd09f7613301958b1ff8b61dfd
Youcef <youcef.laribi@citrix.com>
2013-03-21 16:06:53 -0700

    updated project pom to use the latest versions of hibernate-jpamodelgen artifact. Did not change to latest version of hibernate-entitymanager due to a dramatic increase in execution time (need investigation)

--- bin/seed_dbs.sql
+++ bin/seed_dbs.sql
@@ -1,8 +1,8 @@
 use loadbalancing;
-source /home/youcef/projects/radware-test/pathweaver-lb/core-api/core-public-web/src/deb/contrib/db/core-seed.sql;
+source ./controller/controller-war/src/deb/contrib/db/seed.sql;
 
 use loadbalancingplugin;
-source /home/youcef/projects/radware-test/pathweaver-lb/core-plugins/core-netscaler-plugin/src/main/resources/core-netscaler-plugin-seed.sql;
+source /pathweaver-lb/core-plugins/core-netscaler-plugin/src/main/resources/core-netscaler-plugin-seed.sql;
 
 use helios_loadbalancing;
 insert into tenant (id,name) VALUES('1000', 'tenant1');
